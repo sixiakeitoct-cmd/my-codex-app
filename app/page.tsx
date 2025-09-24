94fa45f8975a91a68039e66452bce43b11e66717
sixiakeitoct-cmd <sixia.keito.ct@gmail.com>
2025-09-24 17:46:48 +0700

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>Codex × Next.js 🚀</h1>
-      <p>これは Next.js(App Router) の最小ページです。</p>
-    </main>
-  );
-}
